38303e8bf08bf765e6feaf7dda493d240a6dae7a
Chris Raye <chrisrayemendoza1@gmail.com>
2024-03-04 15:30:55 +0800

    version 2

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import '../Style/home.css'
 import { FaFacebook } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaGithub } from "react-icons/fa";
 import MyImage from './pic.png'
+const Typewriter = ({ texts }) => {
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const [displayText, setDisplayText] = useState(''); 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (currentIndex === texts[currentTextIndex].length) {
+        clearInterval(intervalId);
+        setTimeout(() => {
+          setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+          setCurrentIndex(0);
+          setDisplayText('');
+        }, 1000); // Change this delay to control how long to wait before displaying the next text
+        return;
+      }
+
+      setDisplayText((prevText) => prevText + texts[currentTextIndex][currentIndex]);
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    }, 100); // Change the speed by adjusting this interval (milliseconds)
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex, currentTextIndex, texts]);
+
+  return <span>{displayText}</span>;
+};
+
 const Home = () => {
+  const texts = ['Web Developer', 'Student', 'Handsome'];
   return (
+
   
    <div className="hmain" id='home'>
         <div className="right">
           <div className="rightc">
     <h1>I am </h1>
     <h1>Chris Raye Mendoza</h1>
-    <h1>Web Developer</h1>
+   <h1><Typewriter texts={texts} /></h1>
+
+
    <div className="icon">
       <a href="https://web.facebook.com/chrisraye.mendoza.7"><FaFacebook className="icons"/></a>
       <a href="https://www.instagram.com/itsyurboychris/" ><AiFillInstagram className='icons'/></a> 
@@ -29,6 +60,18 @@ const Home = () => {
             <img src={MyImage} alt='' className="circle-img"/>
             </div>
         </div>
+        <ul class="background">
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+   <li></li>
+</ul>
  </div>
     
 
